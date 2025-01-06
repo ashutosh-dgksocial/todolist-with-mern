cc088ef6b2baa606445240e9e89e97dcac941eba
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2025-01-06 19:09:29 +0530

    almost

--- frontend_todo/src/components/TodoApp.jsx
+++ frontend_todo/src/components/TodoApp.jsx
@@ -5,29 +5,41 @@ import { RiDeleteBack2Line } from "react-icons/ri";
 const TodoList = () => {
     const [inputData, setInputData] = useState("");
     const [value, setValue] = useState([]);
-    const [isEdit, setIsEdit] = useState(null);
-    const [customIndex, setCustomIndex] = useState("");
+    const [isEdit, setIsEdit] = useState(false);
+    const [customIndex, setCustomIndex] = useState(null);
 
     useEffect(() => {
         // console.log(Array.isArray(value), "this was the array : ", value);
         // console.log("itemIndex;", itemIndex);
-        console.log("your index::", customIndex);
-    }, [customIndex]);
+        // console.log("your index::", customIndex);
+    }, []);
 
     const handleOnchange = (e) => {
         setInputData(e.target.value);
     };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        setValue((prev) => [...prev, inputData]);
-        setInputData("");
-    };
-    const handleEdit = (index) => {
-        setCustomIndex(() => {
-            alert(index)
-            return index;
-        });
+        if (inputData.trim() === "") return;
 
+        if (isEdit) {
+            const updatedValue = value.map((item, index) => index === customIndex ? inputData : item)
+            setValue(updatedValue);
+            setInputData("")
+            setIsEdit(false)
+        }
+        // this is for normal submit i separate using If else this was written first place
+        else {
+            setValue((prev) => [...prev, inputData]);
+            setInputData("");
+        }
+    };
+
+    const handleEdit = (index) => {
+        setCustomIndex(index);
+        const foundItem = value[index]; // this is console the the key not the value not the index
+        setInputData(foundItem);
+        setIsEdit(true);
     };
 
     return (
@@ -36,7 +48,7 @@ const TodoList = () => {
                 Todo List
             </h2>
             <form
-                onSubmit={isEdit ? handleUpdate : handleSubmit}
+                onSubmit={handleSubmit}
                 className="flex mb-4"
             >
                 <input
@@ -61,30 +73,32 @@ const TodoList = () => {
                 </p>
             ) : (
                 <ul className="list-none">
-                    {value.map((data, index) => (
-                        <li
-                            key={index}
-                            className="flex justify-between items-center py-2 border-b border-gray-600 line-clamp-1 text-nowrap "
-                        >
-                            <span className="text-white">
-                                {index + 1}. {data}
-                            </span>
-                            <div>
-                                <button
-                                    onClick={() => handleEdit(index)}
-                                    className="text-green-400 hover:text-green-500 mr-2"
-                                >
-                                    <FiEdit />
-                                </button>
-                                <button
-                                    onClick={() => handleDelete(index)}
-                                    className="text-red-400 hover:text-red-500"
-                                >
-                                    <RiDeleteBack2Line />
-                                </button>
-                            </div>
-                        </li>
-                    ))}
+                    {value.map((data, index) => {
+                        return (
+                            <li
+                                key={index}
+                                className="flex justify-between items-center py-2 border-b border-gray-600 line-clamp-1 text-nowrap "
+                            >
+                                <span className="text-white">
+                                    {index + 1}. {data}
+                                </span>
+                                <div>
+                                    <button
+                                        onClick={() => handleEdit(index)}
+                                        className="text-green-400 hover:text-green-500 mr-2"
+                                    >
+                                        <FiEdit />
+                                    </button>
+                                    <button
+                                        onClick={() => handleDelete(index)}
+                                        className="text-red-400 hover:text-red-500"
+                                    >
+                                        <RiDeleteBack2Line />
+                                    </button>
+                                </div>
+                            </li>
+                        );
+                    })}
                 </ul>
             )}
         </div>
